567084b319788976e899d8fc0c82358c234ec089
Etienne Richard <etienne.jeanrichard@gmail.com>
2023-12-03 01:01:32 +0100

    feat: Build the navigation menu

--- js/app.js
+++ js/app.js
@@ -1,62 +1,65 @@
+// @ts-check
+
 /**
- * 
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
-*/
+ */
+
+/*------------------------------------------------------------------------------------------------
+ * Main part
+ *------------------------------------------------------------------------------------------------*/
+
+/*
+ * Begin main functions.
+ */
 
 /**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
-*/
+ * Builds the navigation bar.
+ */
+function buildNav() {
+  // Get all sections.
+  const sections = document.querySelectorAll('main > section');
+
+  // Create a fragment to hold the menu items.
+  const itemsFragment = document.createDocumentFragment();
+  for (const section of sections) {
+    const sectionDataNav = section.getAttribute('data-nav')
+    const li = document.createElement('li');
+    li.innerHTML = `<a href="#${section.id}" class="menu__link">${sectionDataNav}</a>`;
+    itemsFragment.appendChild(li);
+  }
+
+  // Insert the menu items.
+  const navbarList = document.querySelector('#navbar__list');
+  // @ts-ignore: ... is possibly 'null'.
+  navbarList.replaceChildren(itemsFragment);
+}
 
 /**
- * Define Global Variables
- * 
-*/
+ * Sets up navigation-bar, highlighting the active section, etc.
+ */
+function doSetup() {
+  // Navigation bar.
 
+  // Build the navigation bar.
+  buildNav();
 
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
-
-
+}
 
 /**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
+ * Begin events.
+ */
 
-// build the nav
+document.addEventListener('DOMContentLoaded', _ => doSetup());
 
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
-
-
+/*
+ * End events.
+ */
